0585226ac56ae22d8fe5c61cb0c6d8f1561ca865
Javon Harper <javon.d.harper@gmail.com>
2015-05-06 20:02:46 -0400

    Try to deploy this on heroku again...

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [environ "1.0.0"]
                  [mynx "2.0.0"]
                  [org.clojure/data.json "0.2.5"]
+                 [ring/ring-servlet "1.2.0-RC1"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler goodnotes-reddit-api.core.handler/app}
